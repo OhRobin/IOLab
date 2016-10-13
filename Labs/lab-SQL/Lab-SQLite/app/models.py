b04b5decd93c1ff83e0e5a7046b2c36ab75e113e
Robin Oh <Robin Oh>
2016-10-13 13:21:37 -0700

    adding the lab shit

--- Labs/lab-SQL/Lab-SQLite/app/models.py
+++ Labs/lab-SQL/Lab-SQLite/app/models.py
@@ -1,7 +1,20 @@
 import sqlite3 as sql
 
-def insert_customer():
+def insert_customer(company, email):
     # SQL statement to insert into database goes here
+    with sql.connect("app.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO customers(company,email) VALUES (?,?)",(company, email))
+        con.commit()
+#con = sql.connect("app.db")
+#sql code here
+#con.commit()
+#con.close
+#this is the same as above code
 
 def retrieve_customers():
     # SQL statement to query database goes here
+    with sql.connect("app.db") as con:
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        result = 
--- Labs/lab-SQL/Lab-SQLite/schema.sql
+++ Labs/lab-SQL/Lab-SQLite/schema.sql
@@ -1,2 +1,3 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
+drop table if extists customers
